324365bb279380bebecf7783f25fac82b5773a4d
dakota1599 <dakotashapiro15@gmail.com>
2020-02-20 14:26:54 -0500

    Add base MVC files.

---
+++ core/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+require 'routes.php';
+require 'router.php';
+require 'request.php';
+require 'sql.php';
+require 'file.php';
+
+$router = new Router($routes);
+?>
---
+++ views/partials/header.partial.php
@@ -0,0 +1,7 @@
+<?php //This is the default layout page.
+require 'core/variables.php';
+?>
+
+<html>
+<head></head>
+<body>
